974da35928f7450860f3ef4589f399e69c1821e0
kaiicmelo <kaiicmelo@gmail.com>
2026-04-23 15:52:38 -0300

    Update src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java via Motor Fiscal Setup

--- src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
+++ src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
@@ -9,4 +9,4 @@ public class MotorFiscalApplication {
         SpringApplication.run(MotorFiscalApplication.class, args);
     }
 }
-// Sync: 2026-04-23T18:44:17.519Z
+// Sync: 2026-04-23T18:52:31.808Z
