71d3e5e97ede4b51c618c22b8ca596010049c5ab
kaiicmelo <kaiicmelo@gmail.com>
2026-04-24 11:50:19 -0300

    Deploy PRO v1.0.4 FINAL: src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java

--- src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
+++ src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
@@ -1,2 +1,2 @@
 package com.sellerx.motorfiscal; import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; @SpringBootApplication public class MotorFiscalApplication { public static void main(String[] args) { SpringApplication.run(MotorFiscalApplication.class, args); } }
-// Sync: 2026-04-24T14:47:08.148Z
+// Sync: 2026-04-24T14:50:12.800Z
