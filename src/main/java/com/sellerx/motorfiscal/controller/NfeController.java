c0e133ed855f30ef9673884d90fcb101c92c6d22
kaiicmelo <kaiicmelo@gmail.com>
2026-04-24 11:18:24 -0300

    Deploy Produção Seguro v1.0.1: src/main/java/com/sellerx/motorfiscal/controller/NfeController.java

--- src/main/java/com/sellerx/motorfiscal/controller/NfeController.java
+++ src/main/java/com/sellerx/motorfiscal/controller/NfeController.java
@@ -165,4 +165,4 @@ public class NfeController {
     }
     @GetMapping("/process") public ResponseEntity<?> ping() { return ResponseEntity.ok(Map.of("status", "online")); }
 }
-// Prod Sync: 2026-04-24T13:54:30.366Z
+// Prod Sync: 2026-04-24T14:18:16.024Z
