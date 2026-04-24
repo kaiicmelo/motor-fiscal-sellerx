9afe354afc93adbf72f3cf7352cb6e9dee713140
kaiicmelo <kaiicmelo@gmail.com>
2026-04-24 17:02:32 -0300

    Deploy PRO ABSOLUTE 1.0.5: src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java

--- src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
+++ src/main/java/com/sellerx/motorfiscal/MotorFiscalApplication.java
@@ -1,2 +1,2 @@
 package com.sellerx.motorfiscal; import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; @SpringBootApplication public class MotorFiscalApplication { public static void main(String[] args) { SpringApplication.run(MotorFiscalApplication.class, args); } }
-// Quebra Cache: 2026-04-24T19:53:38.158Z-qlyo2d
+// Quebra Cache: 2026-04-24T20:02:26.176Z-5glz8
